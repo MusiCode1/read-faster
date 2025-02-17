757428781699ad4db7e1e0b425524ae6622abf29
Musicode1 <MusiCode3@gmail.com>
2025-02-17 09:30:23 +0200

    feat(words): עדכון רשימת התמונות הזמינות
    - הוספת סיומת jpeg למערך הסיומות האפשריות
    - עדכון רשימת הקבצים בהתאם לתמונות הזמינות בתיקיית static/images

--- src/lib/data/words.ts
+++ src/lib/data/words.ts
@@ -2,21 +2,29 @@ import type { WordList } from '../types';
 import { WordUtils } from '../utils/wordUtils';
 import wordList from './words.json';
 
-const IMAGE_EXTENSIONS = ['jpg', 'png'] as const;
+const IMAGE_EXTENSIONS = ['jpg', 'png', 'jpeg'] as const;
 
 function getImagePath(word: string): string|false {
   const cleanWord = WordUtils.removeNiqqud(word);
   
   for (const ext of IMAGE_EXTENSIONS) {
     const fileName = `${cleanWord}.${ext}`;
-    const files = ['באה.jpg', 'בכה.jpg', 'בלע.jpg', 'במה.png', 'בנה.png'];
+    const files = [
+      'באה.jpg', 'בכה.jpg', 'בכה.png', 'בלע.jpg', 'במה.png', 'בנה.png',
+      'גרה.png', 'דגה.jpeg', 'זזה.png', 'חדה.png', 'חוה.png', 'חיה.png',
+      'חכה.jpeg', 'חלה.jpeg', 'חמה.jpeg', 'טסה.jpeg', 'כלה.png', 'מרה.png',
+      'נאה.png', 'נגע.png', 'נמה.png', 'ספה.jpeg', 'עלה.png', 'ענה.png',
+      'עפה.png', 'פנה.jpg', 'פרה.jpeg', 'צמה.png', 'קמה.png', 'קנה.png',
+      'קרא.png', 'קרע.png', 'ראה.png', 'רבה.png', 'רזה.png', 'רכה.png',
+      'שטה.png', 'שעה.png', 'שרה.png', 'שתה.png'
+    ];
     
     if (files.includes(fileName)) {
       return `/images/${fileName}`;
     }
   }
   
-  return false
+  return false;
 }
 
 const w = wordList.leavel_1.map((word) => ({
